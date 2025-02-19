a63ff6c390fec1110e70ad6ce3dfc27605180c73
capT <141147918+Kapil1331@users.noreply.github.com>
2025-02-19 22:04:14 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.splitright = true
 vim.opt.scrolloff = 15
+
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('WQ', 'wq', {})
@@ -25,11 +26,20 @@ set_keymap("(", "()<Left>")
 set_keymap("[", "[]<Left>")
 set_keymap("{", "{<CR>}<Esc>ko")
 set_keymap("<", "<><Left>")
-vim.opt.number = true
+
 vim.cmd("runtime plugin/.cscope_maps.vim")
-vim.opt.relativenumber = true
 vim.cmd('colorscheme torte')
+
 vim.opt.tabstop = 6
 vim.opt.shiftwidth = 6
 vim.opt.softtabstop = 6 
---vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+vim.cmd("highlight CursorLine cterm=none")
+vim.cmd("highlight CursorLine guibg=#31363F ctermbg=237")
+
+vim.cmd("highlight CursorLineNr guifg=#FF69B4 ctermfg=201 cterm=none gui=none")
+vim.cmd("highlight LineNr guifg=#FF7F7F ctermfg=210")
